a1199df35e4ca479111e6505f7fd7ff64ced6a6d
riedes <riedes20@gmail.com>
2020-07-07 11:04:59 +0800

    test commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Sye Martinez  on 7/4/20.
+//  Copyright © 2020 Sye Martinez . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        //test changes to the code
+    }
+
+
+}
+
