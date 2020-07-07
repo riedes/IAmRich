8b60994815510c4ace60eced809eada73869355c
riedes <riedes20@gmail.com>
2020-07-07 11:27:24 +0800

    test commit part two

--- I Am Rich/ViewController.swift
+++ I Am Rich/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         //test changes to the code
+        
+        //next commit
     }
 
 
